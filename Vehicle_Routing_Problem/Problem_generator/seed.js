28e5f9b310609edfe6fe0446ca3960ecea9ba0eb
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2022-11-22 17:25:57 +0100

    Express and new arg add

--- Vehicle_Routing_Problem/Problem_generator/seed.js
+++ Vehicle_Routing_Problem/Problem_generator/seed.js
@@ -1,15 +1,12 @@
 const seed = require("random-seed");
 
-function generator(size, citiesDistance_x, citiesDistance_y){
+function generator(size, citiesDistance_x, citiesDistance_y, seedData){
 
-    var array = [{}];
+    var array = [{}]
 
-    const date = new Date;
-    const dateInSec = date.getHours()*60*60+date.getMinutes()*60+date.getSeconds();     //SEED
+    const seedInit = seed.create()
 
-    const seedInit = seed.create();
-
-    seedInit.seed('a')
+    seedInit.seed(seedData)
     for(i = 0; i < size; i++) {
         array[i] = {
             x: seedInit(citiesDistance_x),
--- Vehicle_Routing_Problem/index.js
+++ Vehicle_Routing_Problem/index.js
@@ -1,12 +1,26 @@
+const express = require("express");
+const app = express();
+app.use(express.json())
+const PORT = 4500;
 const { generator } = require("./Problem_generator/seed");
 const { solution } = require("./Problem_generator/solution_generator");
 
-const routePlanning = ((citiesCount, citiesDistance_x, citiesDistance_y, driverCount, generationCount) =>{
-    const records = generator(citiesCount, citiesDistance_x, citiesDistance_y);
+const routePlanning = ((citiesCount, citiesDistance_x, citiesDistance_y, driverCount, generationCount, seedData) =>{
+    const records = generator(citiesCount, citiesDistance_x, citiesDistance_y, seedData);
 
     return solution(records,driverCount, generationCount);
 })
 
-module.exports = {
-    routePlanning,
-}
+
+app.get('/', (req, res) =>{
+    const {citiesCount, citiesDistance_x, citiesDistance_y, driverCount, generationCount, seedData} = req.body
+    res.status(200).send(routePlanning(citiesCount, citiesDistance_x, citiesDistance_y, driverCount, generationCount, seedData))
+})
+
+var server = app.listen(PORT, function(){
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Server is running at : http://%s:%s", host, port)
+    console.log("Server is running at : http://127.0.0.1:%s", port)
+})
