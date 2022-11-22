f6817177beec4f3d42ee1d1c7a4166ab4a79f07b
K-Tamas01 <79214295+K-Tamas01@users.noreply.github.com>
2022-11-22 22:16:54 +0100

    webszerver init
    Cors hozzáadás

--- Vehicle_Routing_Problem/index.js
+++ Vehicle_Routing_Problem/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors")
 const app = express();
 app.use(express.json())
 const PORT = 4500;
@@ -6,13 +7,12 @@ const { generator } = require("./Problem_generator/seed");
 const { solution } = require("./Problem_generator/solution_generator");
 
 const routePlanning = ((citiesCount, citiesDistance_x, citiesDistance_y, driverCount, generationCount, seedData) =>{
-    const records = generator(citiesCount, citiesDistance_x, citiesDistance_y, seedData);
-
-    return solution(records,driverCount, generationCount);
+    return solution(generator(citiesCount, citiesDistance_x, citiesDistance_y, seedData),driverCount, generationCount);
 })
 
+app.use(cors())
 
-app.get('/', (req, res) =>{
+app.post('/', (req, res) =>{
     const {citiesCount, citiesDistance_x, citiesDistance_y, driverCount, generationCount, seedData} = req.body
     res.status(200).send(routePlanning(citiesCount, citiesDistance_x, citiesDistance_y, driverCount, generationCount, seedData))
 })
